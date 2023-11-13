de547f65c38e7fef49c8fa70ce17afc45893fd4b
Dayo <dayo@dojah.io>
2023-11-13 10:03:00 +0100

    added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import {tomatoGrotesk} from '../../public/fonts';
 import Nav from '@/components/nav';
 import Footer from '@/components/footer';
 import './globals.css';
+import Script from 'next/script';
 
 const metadata: Metadata = {
   openGraph: {
@@ -14,6 +15,17 @@ const metadata: Metadata = {
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_APP_GA_MEASUREMENT_ID}`}
+      />
+      <Script id="google-analytics">
+        {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_APP_GA_MEASUREMENT_ID}');
+        `}
+      </Script>
       <body
         className={`${tomatoGrotesk.className} min-h-screen bg-white px-4 lg:px-[25px] pt-2 pb-6 flex flex-col`}
       >
