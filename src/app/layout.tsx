0917e59a1add306ec8ed831d5fc817eb29a79844
Dayo <dayo@dojah.io>
2023-11-13 10:09:40 +0100

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
       <Script
+        async
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_APP_GA_MEASUREMENT_ID}`}
       />
       <Script id="google-analytics">
