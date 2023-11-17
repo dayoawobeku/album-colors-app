c673642137cb746dbb83ce69489c6b79258238b1
Dayo <dayo@dojah.io>
2023-11-17 08:50:31 +0100

    truncated genre x album texts

--- src/app/album/[id]/page.tsx
+++ src/app/album/[id]/page.tsx
@@ -59,14 +59,18 @@ export default async function AlbumPage({params}: {params: {id: string}}) {
     (album: Album) => album.album_id === id,
   );
 
+  const truncate = (str: string, n: number) => {
+    return str.length > n ? str.substring(0, n - 1) + '...' : str;
+  };
+
   return (
     <main className="grow mt-16 sm:mt-24 flex flex-col lg:flex-row items-stretch lg:items-center justify-between basis-full xl:px-6 gap-4 xl:gap-[5.42%]">
       <div className="basis-full lg:basis-1/3">
         <NextPrevAlbum data={data || []} params={params} />
 
         <div className="mt-6 space-y-4 xl:space-y-10">
-          <div className="border border-1 border-grey-500 px-2 lg:px-6 xl:px-12 py-4 relative flex items-center justify-between">
-            <div className="h-[0.5px] w-full bg-grey-500 absolute -mx-2 lg:-mx-6 xl:-mx-12" />
+          <div className="border border-1 border-grey-500 px-2 lg:px-6 2xl:px-4 py-4 relative flex items-center justify-between">
+            <div className="h-[0.5px] w-full bg-grey-500 absolute -mx-2 lg:-mx-6 xl:-mx-4" />
             <div className="flex flex-col items-center justify-between">
               <p className="uppercase font-bold text-sm text-grey pb-4 text-center">
                 artist
@@ -79,13 +83,16 @@ export default async function AlbumPage({params}: {params: {id: string}}) {
               <p className="uppercase font-bold text-sm text-grey pb-4 text-center">
                 album title
               </p>
-              <p className="text-grey-700 font-semibold text-sm pt-4 text-center">
+              <p className="hidden truncated-text text-grey-700 font-semibold text-sm pt-4 text-center px-4">
                 {album.album_title}
+              </p>
+              <p className="sm:hidden text-grey-700 font-semibold text-sm pt-4 text-center">
+                {truncate(album.album_title, 30)}
               </p>
             </div>
           </div>
-          <div className="border border-1 border-grey-500 px-2 lg:px-6 xl:px-12 py-4 relative flex items-center justify-between">
-            <div className="h-[0.5px] w-full bg-grey-500 absolute -mx-2 lg:-mx-6 xl:-mx-12" />
+          <div className="border border-1 border-grey-500 px-2 lg:px-6 2xl:px-4 py-4 relative flex items-center justify-between">
+            <div className="h-[0.5px] w-full bg-grey-500 absolute -mx-2 lg:-mx-6 xl:-mx-4" />
             <div className="flex flex-col items-center justify-between">
               <p className="uppercase font-bold text-sm text-grey pb-4 text-center">
                 year
@@ -98,10 +105,18 @@ export default async function AlbumPage({params}: {params: {id: string}}) {
               <p className="uppercase font-bold text-sm text-grey pb-4 text-center">
                 genre
               </p>
-              <p className="text-grey-700 font-semibold text-sm pt-4 text-center">
+              <p className="hidden truncated-text text-grey-700 font-semibold text-sm pt-4 text-center px-4">
                 {album.genres
                   .map(genre => genre[0].toUpperCase() + genre.slice(1))
                   .join(', ') || '-'}
+              </p>
+              <p className="sm:hidden text-grey-700 font-semibold text-sm pt-4 text-center">
+                {truncate(
+                  album.genres
+                    .map(genre => genre[0].toUpperCase() + genre.slice(1))
+                    .join(', ') || '-',
+                  30,
+                )}
               </p>
             </div>
           </div>
